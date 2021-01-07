2f935bee76294e2906151bf21f4bbbef175704c4
swoga <swoga@users.noreply.github.com>
2021-01-07 19:26:07 +0100

    feat: extend config

--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 type Config struct {
-	Listen      string            `yaml:"listen"`
-	ProbePath   string            `yaml:"probe_path"`
-	MetricsPath string            `yaml:"metrics_path"`
-	Timeout     float64           `yaml:"timeout"`
-	Devices     map[string]Device `yaml:"devices"`
+	Listen      string             `yaml:"listen"`
+	ProbePath   string             `yaml:"probe_path"`
+	MetricsPath string             `yaml:"metrics_path"`
+	Timeout     float64            `yaml:"timeout"`
+	Devices     map[string]*Device `yaml:"devices"`
+	Global      Global             `yaml:"global"`
 }
 
 func DefaultConfig() Config {
@@ -14,6 +15,17 @@ func DefaultConfig() Config {
 		ProbePath:   "/probe",
 		MetricsPath: "/metrics",
 		Timeout:     60,
+		Global: Global{
+			Options: DefaultOptions(),
+		},
+	}
+}
+
+func DefaultOptions() Options {
+	return Options{
+		ExportOLT:      true,
+		ExportONUs:     true,
+		ExportMACTable: false,
 	}
 }
 
@@ -25,11 +37,47 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return err
 	}
 
+	for _, device := range c.Devices {
+		if device.Username != nil {
+			device.Username = &c.Global.Username
+		}
+		if device.Password != nil {
+			device.Password = &c.Global.Password
+		}
+		if device.Options == nil {
+			device.Options = &c.Global.Options
+		}
+	}
+
+	return nil
+}
+
+type Global struct {
+	Username string  `yaml:"username"`
+	Password string  `yaml:"password"`
+	Options  Options `yaml:"options"`
+}
+
+type Options struct {
+	ExportOLT      bool `yaml:"export_olt"`
+	ExportONUs     bool `yaml:"export_onus"`
+	ExportMACTable bool `yaml:"export_mac_table"`
+}
+
+func (o *Options) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	*o = DefaultOptions()
+
+	type plain Options
+	if err := unmarshal((*plain)(o)); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 type Device struct {
-	Address  string `yaml:"address"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
+	Address  string   `yaml:"address"`
+	Username *string  `yaml:"username"`
+	Password *string  `yaml:"password"`
+	Options  *Options `yaml:"options"`
 }
